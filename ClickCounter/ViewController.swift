3882afb6df6c42ca35a300839ea206ae44a54c6b
Marco AV Silva <mavs57@me.com>
2018-10-15 16:41:36 -0300

    Following Udacity iOS Nanodegree, created another Label, incremented it after the first Label and then added a decrement Button

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     var counter = 0
+    var anotherCounter = 0
     var label: UILabel!
+    var anotherLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,23 +20,48 @@ class ViewController: UIViewController {
         
         // Label
         let label = UILabel()
+        let anotherLabel = UILabel()
+        
         label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
+        anotherLabel.frame = CGRect(x: 250, y: 150, width: 60, height: 60)
+        anotherLabel.text = "0"
+        
         view.addSubview(label)
         self.label = label
+        view.addSubview(anotherLabel)
+        self.anotherLabel = anotherLabel
         
-        //button
-        let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(button)
+        // increment incrButton
+        let incrButton = UIButton()
+        incrButton.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        incrButton.setTitle("Incr", for: .normal)
+        incrButton.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(incrButton)
+        incrButton.addTarget(self, action: #selector(ViewController.incrementCount), for: .touchUpInside)
         
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: .touchUpInside)
+        // decrement incrButton
+        let decrButton = UIButton()
+        decrButton.frame = CGRect(x: 150, y: 350, width: 60, height: 60)
+        decrButton.setTitle("Decr", for: .normal)
+        decrButton.setTitleColor(UIColor.red, for: .normal)
+        view.addSubview(decrButton)
+        decrButton.addTarget(self, action: #selector(ViewController.decrementCount), for: .touchUpInside)
     }
 
     @objc func incrementCount() {
         self.counter += 1
+        self.label.text = "\(self.counter)"
+        incrementAnotherLabel()
+    }
+
+    @objc func incrementAnotherLabel() {
+        self.anotherCounter += 1
+        self.anotherLabel.text = "\(self.anotherCounter)"
+    }
+
+    @objc func decrementCount() {
+        self.counter -= 1
         self.label.text = "\(self.counter)"
     }
 
